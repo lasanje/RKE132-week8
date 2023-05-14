caabe1c87eb1aa35d4e8b347d6565cb3c8fc09da
SP <spukk@tktk.ee>
2023-05-14 13:29:51 +0300

    add week8 code

---
+++ RKE132-week8/Program.cs
@@ -0,0 +1,27 @@
+﻿string folderPath = @"C:\data\";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heros = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+
+//string[] heros = { "harry potter", "Luke", "lara", "scooby" };
+//string[] villains = { "voldemor", "drath", "dracula", "joker", "sauron" };
+string[] weapon = { "õun", "banaan", "püss", "kahvel" };
+
+
+string hero = GetRandomValueFromArray(heros);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero}´with {heroWeapon} saves the day");
+
+string villain = GetRandomValueFromArray(villains);
+string villianWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villains} with {villianWeapon} tries to take over the world");
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
